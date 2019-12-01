1510a2aeb434c48ea3edfc7ae8589e0aa692599a
Ali Gasymov <alik0211alik@gmail.com>
2019-12-01 14:39:23 +0400

    Optimize render in canvas

--- script.js
+++ script.js
@@ -18,33 +18,59 @@ document.querySelectorAll('.templates__item').forEach(item => {
 });
 
 function renderPixels(url) {
-  fetch(url).then(response => response.json()).then(data => {
-    let canvas = document.querySelector("canvas");
-    var ctx = canvas.getContext('2d');
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    let width = data.length,
-    height = data.length,
-    scale = 512 / data.length;
-    canvas.width = width * scale;
-    canvas.height = height * scale;
-    for (let row = 0; row < height; row++) {
-      for (let col = 0; col < width; col++) {
-        ctx.fillStyle = getColor(data[row][col]);
-        ctx.fillRect(col * scale, row * scale, scale, scale);
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      let canvas = document.querySelector('canvas');
+      var ctx = canvas.getContext('2d');
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      let width = data.length,
+        height = data.length,
+        scale = 512 / data.length;
+      canvas.width = width * scale;
+      canvas.height = height * scale;
+
+      let count = 0;
+
+      for (let x = 0; x < Math.ceil(height / 2); x++) {
+        for (let y = 0; y < Math.ceil(width / 2); y++) {
+          const x2 = height - x - 1;
+          const y2 = width - y - 1;
+
+          ctx.fillStyle = getColor(data[x][y]);
+          ctx.fillRect(y * scale, x * scale, scale, scale);
+
+          ctx.fillStyle = getColor(data[x][y2]);
+          ctx.fillRect(y2 * scale, x * scale, scale, scale);
+
+          ctx.fillStyle = getColor(data[x2][y]);
+          ctx.fillRect(y * scale, x2 * scale, scale, scale);
+
+          ctx.fillStyle = getColor(data[x2][y2]);
+          ctx.fillRect(y2 * scale, x2 * scale, scale, scale);
+
+          count += 1;
+        }
+
+        // debugger;
       }
-    }
-  });
+
+      // 1024 -> O(n)
+      // 512 -> O(n / 2) -> O(n)
+
+      console.log('count:', count);
+    });
 }
 
 function renderImage(url) {
-  let canvas = document.querySelector("canvas");
+  let canvas = document.querySelector('canvas');
   let ctx = canvas.getContext('2d');
   let img = new Image();
 
   img.onload = draw;
   img.src = url;
 
-  function draw(){
+  function draw() {
     ctx.drawImage(img, 0, 0, 512, 512);
   }
 }
@@ -79,8 +105,6 @@ function getColor(item) {
 //         }
 //     }
 // };
-
-
 
 // size32.onclick = function() {
 //     width = logo[0].length;
